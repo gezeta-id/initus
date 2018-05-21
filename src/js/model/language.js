92f99d7f3be361f1b69ccbb5a35766be2695aeec
Kevin Flynn <kflynn@lpqf.net>
2018-05-21 23:33:23 +0200

    A fragment based parser.
    Sentence definition has become fragment-based. While the current state
    is just an initial implementation, this approach _is_ the way to go with
    the parser.

--- src/js/model/language.js
+++ src/js/model/language.js
@@ -1,84 +1,45 @@
-let pf = {
-    isKw: w => (w.replace(/^\(/,'').substr(0,1) === ':'),
-    openOpc: w => (w.substr(0,1) === '('),
-    cleanWord: w => w.replace(/^\(?:?|\)*$/g, '')
-};
-function compile(pattern) {
-    let words = pattern.split(' ');
-    let positions = {};
+let { uniqueId } = require('../util/');
+let { compile, matches, extract } = require('./lang-helpers.js');
 
-    let expression = [];
-    let offset=0;
-    let opens = 0;
 
-    for (let p = 0; p < words.length; p++) {
-        let fragment = '';
-        let w = words[p];
-        if (pf.openOpc(w)) {
-            opens +=1;
-            offset+=1;
-            fragment += '(';
-        }
-        if(pf.isKw(w)) {
-            if(positions[pf.cleanWord(w)] === undefined) positions[pf.cleanWord(w)] = [];
-            positions[pf.cleanWord(w)].push(p+1+offset);
-            fragment += '(\\w+)';
-        } else {
-            fragment += '('+pf.cleanWord(w)+')';
-        }
-        expression.push(fragment);
-    }
-    expression = expression.join('\\s+');
-    while(opens--) expression+=')?';
-
-    return {
-        kw: pf.cleanWord(words[0]),
-        re: new RegExp(expression),
-        keys: positions
-    };
-
-}
 
 const commandStore = new WeakMap();
 const commands = t => commandStore.get(t); // lazy boy concession
 
-const id = pattern => pattern.toString();
-
-const matches = input => c => c.checker.re.test(input);
-const extract = (checker, input) => {
-    let matches = input.match(checker.re);
-    return [checker.kw, Object.keys(checker.keys).reduce((k,key) => {
-        k[key] = checker.keys[key].map(pos=>matches[pos]);
-        if(k[key].length === 1) k[key] = k[key][0];
-        return k;
-    }, {})];
-};
+const id = sentence => uniqueId(sentence.command.toString());
 
 class Language {
     constructor() {
         commandStore.set(this, []);
     }
 
-    understand(pattern, handler) {
+    understand(sentence, handler) {
         commands(this).push({
-            id: id(pattern), 
-            checker: compile(pattern),
+            id: id(sentence),
+            checker: compile(sentence),
             handler: handler
         });
         return this;
     }
 
-    forget(pattern) {
-        commands(this) = commands(this).filter((c) => c.id !== id(pattern));
+    forget(sentence) {
+        commands(this) = commands(this).filter((c) => c.id !== id(sentence));
         return this;
     }
 
     act(input) {
+        let matcher = matches(input);
+        let extractor = extract(input);
         commands(this)
-            .filter(matches(input))
+            .filter(matcher)
             .forEach((c) => {
-                c.handler.apply(null,extract(c.checker, input));
+                c.handler.apply(null,extractor(c.checker));
             });
+        return this;
+    }
+
+    load(lang) {
+        lang.forEach(c => this.understand(c.sentence, c.handler));
         return this;
     }
 }
--- src/test/10-language-spec.js
+++ src/test/10-language-spec.js
@@ -1,23 +1,87 @@
-var 
+const 
     sinon = require('sinon'),
     chai = require('chai'),
     expect = require('chai').expect;
-var sinonChai = require("sinon-chai");
+const sinonChai = require("sinon-chai");
 
 chai.use(sinonChai);
 
-var Language = require('../js/model/language.js');
+const Language = require('../js/model/language.js');
+
+
+const sentences = {
+    nocaptures: {
+        command: 'wait'
+    },
+    simple: {
+        command: 'say',
+        fragments: [
+            {
+                required: true,
+                capture: ':word'
+            }
+        ]
+    },
+    multicapture: {
+        command: 'say',
+        fragments: [
+            {
+                required: true,
+                capture: ':message*'
+            }
+        ]
+    },
+    extended: {
+        command: 'take',
+        synonyms: ['pick', 'get'],
+        fragments: [
+            {
+                required: true,
+                direct: true,
+                capture: ':thing'
+            },
+            {
+                required: false,
+                capture: 'from :container'
+            }
+        ]
+    }
+};
 
 describe('Language', function () {
     it('...can be instantiated', function() {
         expect(new Language()).not.to.be.null;
     });
-    it('...understands patterns and acts on input', function() {
+    it('...understands a command with no captures', function() {
         let h = sinon.stub();
 
         let l = new Language();
-        l.understand("say :salutation", h);
+        l.understand(sentences.nocaptures, h);
+        l.act("wait");
+        expect(h).to.have.been.calledWithExactly('wait', {});
+    });
+    it('...understands simple commands with simple captures', function() {
+        let h = sinon.stub();
+
+        let l = new Language();
+        l.understand(sentences.simple, h);
         l.act("say hello");
-        expect(h).to.have.been.calledWithExactly('say', {'salutation': 'hello'});
+        expect(h).to.have.been.calledWithExactly('say', {'word': 'hello'});
+    });
+    it('...understands simple commands with a single multicapture', function() {
+        let h = sinon.stub();
+
+        let l = new Language();
+        l.understand(sentences.multicapture, h);
+        l.act("say hello and other things");
+        expect(h).to.have.been.calledWithExactly('say', {'message': ['hello', 'and', 'other', 'things']});
+    });
+    it('...understands simple commands with multiple captures', function() {
+        let h = sinon.stub();
+
+        let l = new Language();
+        l.understand(sentences.extended, h);
+        l.act("take drink from fridge");
+        expect(h).to.have.been.calledWithExactly('take', {'thing': 'drink', 'container': 'fridge' });
     });
 });
